3881a4405e9d09ca62a9dbfb910fb3ec40efd579
Neeraj Choithwani <neerajchoithwani28@gmail.com>
2026-01-03 16:17:18 +0530

    Fix Azure Functions workflow and API integration

--- api/getMenu/index.js
+++ api/getMenu/index.js
@@ -2,8 +2,8 @@ module.exports = async function (context, req) {
   context.res = {
     status: 200,
     body: [
-      { id: 1, name: "Healthy Veg Bowl", price: 199 },
-      { id: 2, name: "Chicken Protein Meal", price: 299 },
+      { id: 1, name: "Veg Bowl", price: 199 },
+      { id: 2, name: "Chicken Meal", price: 299 },
       { id: 3, name: "Vegan Delight", price: 249 }
     ]
   };
